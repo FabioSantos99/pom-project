dd278f71e5ecdace1edf1e2d19cce718d1ad1d9e
Fabio Santos <fsantos93x@gmail.com>
2025-11-22 02:14:19 -0300

    children and final components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 
+import { TimerIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  console.log('Oi');
-
   return (
-
   <>
-  <Heading attr={123} attr2={'String'}>Olá mundo!</Heading>
+  <Heading>Olá mundo!
+    <button>
+          <TimerIcon />
+    </button>
+  </Heading>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet, deserunt sint, delectus, at asperiores dolores cupiditate magni odit fugit error? Sed porro explicabo hic repudiandae magni nihil error veniam.</p>
     </>
-  )
+  );
 }
