0ce98bd3de8dd89f667645375d22bcc598a83688
Fabio Santos <fsantos93x@gmail.com>
2025-11-23 02:41:44 -0300

    Moving Container to Own Component React

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
@@ -7,12 +6,25 @@ import './styles/theme.css';
 export function App() {
   return (
   <>
-  <Heading>Olá mundo!
-    <button>
-          <TimerIcon />
-    </button>
+  <Heading>
+    Testando Componente
   </Heading>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet, deserunt sint, delectus, at asperiores dolores cupiditate magni odit fugit error? Sed porro explicabo hic repudiandae magni nihil error veniam.</p>
-    </>
+  <Container>
+    <section>LOGO</section>
+  </Container>
+
+    <Container>
+    <section>Menu</section>
+  </Container>
+
+    <Container>
+    <section>Footer</section>
+  </Container>
+
+    <Container>
+    <section>LOGO</section>
+  </Container>
+  
+  </>
   );
 }
