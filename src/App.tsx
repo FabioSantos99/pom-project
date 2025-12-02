a312512b5bf9210380f81b69e9c63a859cc2876b
Fabio Santos <fsantos93x@gmail.com>
2025-12-02 03:09:17 -0300

    update!

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './styles/global.css';
 import './styles/theme.css';
-
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -9,11 +8,30 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
+import { Heading } from './components/Heading';
 
 
 export function App() {
+  let numero = 0;
+
+  function handleClick() {
+    const span = document.getElementById('numero');
+
+    if (!span) return;
+
+    numero += 1;
+    span.innerText = numero.toString();
+    console.log(numero, Date.now())
+  }
+
   return (
   <>
+  <Heading>
+    Número: <span id='numero'>{numero}</span>
+  </Heading>
+  <button onClick={handleClick}>Aumenta</button>
+  
   <Container>
     <Logo />
   </Container>
@@ -30,7 +48,7 @@ export function App() {
     <form className='form' action="">
       <div className='formRow'>
         <DefaultInput 
-        labelText='task' 
+        labelText={numero.toString()} 
         id='meuInput' 
         type='text'
         placeholder='Digite algo'
@@ -55,10 +73,14 @@ export function App() {
       </div>
 
       <div className='formRow'>
-        <DefaultButton icon={<PlayCircleIcon />}/>
+        <DefaultButton icon={<PlayCircleIcon />} color='green'/>
       </div>
     </form>
     
+  </Container>
+
+  <Container>
+    <Footer />
   </Container>
   </>
   );
