2bb27ee70e1702b999b9a8641181511d9041a947
Fabio Santos <fsantos93x@gmail.com>
2025-12-11 03:57:13 -0300

    pages and GenericHtml

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,11 @@
+import { AboutPomodoro } from './pages/AboutPomodoro';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 import './styles/global.css';
 import './styles/theme.css';
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
+
 
 
 export function App() {
-  return (
-  <>
- 
-  <Container>
-    <Logo />
-  </Container>
-
-    <Container>
-      <Menu/>
-  </Container>
-
-   <Container>
-    <CountDown />
-  </Container>
-
-  <Container>
-    <form className='form' action="">
-      <div className='formRow'>
-        <DefaultInput 
-        labelText='task'
-        id='meuInput' 
-        type='text'
-        placeholder='Digite algo'
-       
-        />
-      </div>
-
-       <div className='formRow'>
-        <p>Lorem ipsum dolor sit amet.</p>
-      </div>
-
-      <div className='formRow'>
-     
-      </div>
-
-      <div className='formRow'>
-        <Cycles />
-      </div>
-
-      <div className='formRow'>
-        <button>Enviar</button>
-      </div>
-
-      <div className='formRow'>
-        <DefaultButton icon={<PlayCircleIcon />} color='green'/>
-      </div>
-    </form>
-    
-  </Container>
-
-  <Container>
-    <Footer />
-  </Container>
-  </>
-  );
+  return <AboutPomodoro />
 }
