0d41fc768897fee017cedf4998cec800b81ba43c
Fabio Santos <fsantos93x@gmail.com>
2025-12-14 02:53:30 -0300

    Define models

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { AboutPomodoro } from './pages/AboutPomodoro';
-import { Home } from './pages/Home';
-import { NotFound } from './pages/NotFound';
 import './styles/global.css';
 import './styles/theme.css';
 
