b16457f4c30e5d4886c0c308ca4dcd0a443f9056
Fabio Santos <fsantos93x@gmail.com>
2025-12-16 03:34:55 -0300

    Send state by props by many components levels

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import { AboutPomodoro } from './pages/AboutPomodoro';
+
+import { useState } from 'react';
+import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+// export type TaskStateModel = {
+//   tasks: TaskModel[]; // histórico
+//   secondsRemaining: number;  // Home, CountDown, Histórico, MainForn,Button
+//   formattedSecondsRemaining: string; // Título, CountDown
+//   activeTask: TaskModel | null; // CountDown, Histórico, MainForm, Button
+//   currentCycle: number; // Home
+//   config: {
+//     workTime: number; // MainForm
+//     shortBreakTime: number; // MainForm
+//     longBreakTime: number; // MainForm
+//   };
+// };
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config:  {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+}
 
 
 
 export function App() {
-  return <AboutPomodoro />
+  const [state, setState] = useState(initialState);
+
+  return <Home  state={state} setState={setState} />;
 }
