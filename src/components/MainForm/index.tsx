2a5703579e30cca2e4885bd0b4cba4b822795fcc
Fabio Santos <fsantos93x@gmail.com>
2026-01-12 02:11:54 -0300

    Format seconds to minutes

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,6 +7,7 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
 
@@ -44,7 +45,7 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
+      duration: state.config[nextCycleType],
       type: nextCycleType,
     };
 
@@ -57,7 +58,7 @@ export function MainForm() {
         activeTask: newTask,
         currentCycle: nextCycle, // conferir
         secondsRemaining, // Conferir
-        formattedSecondsRemaining: '00:00', // Conferir
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), // Conferir
         tasks: [...prevState.tasks, newTask],
 
       }
