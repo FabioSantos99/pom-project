f39aecb49a05d9fb58b6beea086647f7a3eb6f9b
Fabio Santos <fsantos93x@gmail.com>
2026-02-09 02:51:29 -0300

    capture onSubmit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,7 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
-import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplates";
-
-export type HomeProps = {
-  state: TaskStateModel,
-  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>
-}
-
 
 export function Home() {
 
